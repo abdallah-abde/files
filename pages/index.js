a586129cd9f9f9d2d3c21500a653201e2e26a878
abdallah abde <abdallah.abde@gmail.com>
2021-11-05 10:45:50 +0200

    add tailwind and postcss to the project, and configure them, and remove home.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Hello from Home</div>;
+  return (
+    <div>
+      <h1>Hello from Home</h1>
+    </div>
+  );
 }
